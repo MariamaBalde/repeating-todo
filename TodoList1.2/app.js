f76f28a6f0865ba440472bda8369d622e1eac93c
Mariama Balde <foulah18@gmail.com>
2024-08-01 08:53:36 +0000

    commit TodoList1.2

---
+++ TodoList1.2/app.js
@@ -0,0 +1,34 @@
+class TodoList2 extends React.Component{
+constructor(props){
+    super(props)
+    this.state={
+        input:"",
+        todos:[]
+    }
+}
+
+
+render(){
+    return(
+        <div className="container mt-4">
+            <h1>To-Do-List</h1>
+            <div className="input-group mb-3">
+                <input type="text" value={this.state.input} onChange={(e)=>{
+                    this.setState({ input: e.target.value })
+                }} className="form-control" placeholder="Ajoutez une tache"/>
+                <button className="btn btn-primary" onClick={()=>{
+                    this.setState({todos:[...this.state.todos,this.state.input]})
+                }}>Ajouter</button>
+                <p>{this.state.input}</p>
+            </div>
+            <ul className="list-group">
+                {this.state.todos.map((todo)=>{
+                    return <li>{todo}</li>
+                })}
+            </ul>
+        </div>
+    )
+}
+}
+
+ReactDOM.render(<TodoList2/>,document.getElementById("root"))
